b61e40aa811f7375302e2d1785b22441908589b5
Damjan Miloshevski <d.miloshevski@gmail.com>
2021-04-06 14:08:23 +0200

    Finished dark mode

--- app/src/main/java/com/twoplaytech/drbetting/SplashActivity.kt
+++ app/src/main/java/com/twoplaytech/drbetting/SplashActivity.kt
@@ -32,6 +32,5 @@ class SplashActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         startActivity(Intent(this, MainActivity::class.java))
-        finish()
     }
 }
