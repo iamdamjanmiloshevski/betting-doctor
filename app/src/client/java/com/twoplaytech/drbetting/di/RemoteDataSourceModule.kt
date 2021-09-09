2d11fdb5d83c3911b6cd8d186b6b8cf7a5f9fc99
Damjan Miloshevski <d.miloshevski@gmail.com>
2021-09-09 16:35:22 +0200

    Client product flavor refactor

---
+++ app/src/client/java/com/twoplaytech/drbetting/di/RemoteDataSourceModule.kt
@@ -0,0 +1,47 @@
+/*
+ * MIT License
+ *
+ * Copyright (c)  2021 Damjan Miloshevski
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.twoplaytech.drbetting.di
+
+import com.twoplaytech.drbetting.data.datasource.RemoteDataSource
+import com.twoplaytech.drbetting.data.datasource.RemoteDataSourceImpl
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+/*
+    Author: Damjan Miloshevski 
+    Created on 24.8.21 10:57
+    Project: Dr.Betting
+    © 2Play Tech  2021. All rights reserved
+*/
+@Module
+@InstallIn(SingletonComponent::class)
+interface RemoteDataSourceModule {
+    @Binds
+    fun bindRemoteDataSource(
+        remoteDataSourceImpl: RemoteDataSourceImpl
+    ): RemoteDataSource
+}
